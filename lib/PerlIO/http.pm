af3b98f6b2e76d782e3f16f795602eaddc7d60c3
Leon Timmermans <fawaka@gmail.com>
2013-03-12 16:38:07 +0100

    Add documentation

--- lib/PerlIO/http.pm
+++ lib/PerlIO/http.pm
@@ -9,3 +9,27 @@ XSLoader::load(__PACKAGE__, __PACKAGE__->VERSION);
 1;
 
 #ABSTRACT: HTTP filehandles
+
+=encoding utf8
+
+=head1 SYNOPSIS
+
+ open my $fh, '<:http', 'https://metacpan.org/recent';
+
+=head1 DESCRIPTION
+
+This layer enables one to read a file from the internet.
+
+=head1 SYNTAX
+
+This modules does not have to be loaded explicitly, it will be loaded automatically by using it in an open mode. The layer does not take any argument.
+
+=head1 PHILOSPHY
+
+This modules tried to Do The Right Thing™. HTTP errors are translated as faithfully as possible to an C<$!> value (Error C<404> becomes C<ENOENT>, C<403> becomes C<EACCESS>, et cetera…).
+
+=head1 RATIONALE
+
+This module is mostly meant as a proof of concept. It currently does not support writing in an way, and may never support it.
+
+=cut
